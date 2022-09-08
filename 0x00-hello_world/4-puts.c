b8501d7be4dc3867fb844f40ba11d81758ceaa70
jackjavi <jackmtembete@gmail.com>
2022-09-08 07:55:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
+/**
+ * main - Entry point
+ * return 0 - success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
