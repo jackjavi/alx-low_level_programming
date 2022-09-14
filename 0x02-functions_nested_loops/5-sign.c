40f47e684fc4b516e678994a6cb3a67ccb501f4e
jackjavi <jackmtembete@gmail.com>
2022-09-14 05:10:42 -0700

    Check sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - A function that prints the sign of a number.
- * @c - The number to be checked if positive or negative.
+ * @n - The number to be checked if positive or negative.
  *
  * Return - Return 1 if positive, 0 if zero and -1 if negative.
  */
