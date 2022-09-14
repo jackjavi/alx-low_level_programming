40b150e8554fc2f8b7eaedfe529d41e22b545951
jackjavi <jackmtembete@gmail.com>
2022-09-14 01:36:08 -0700

    Print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/**
+ * File : 1-alphabet.c
+ * Auth : Jack Mtembete
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
