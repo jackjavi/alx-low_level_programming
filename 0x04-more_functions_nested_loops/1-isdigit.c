1b108021a93df1a0ae2e1764c9019cfb164dfe4a
jackjavi <jackmtembete@gmail.com>
2022-09-15 13:08:51 -0700

    Fixed error

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,15 +8,10 @@
  */
 int _isdigit(int c)
 {
-	char i;
-
-	for (i = '0'; i <= '9'; i++)
-	{
-		if (i == c)
-			return (1);
-		else
-			return (0);
-	}
+	if ((c >= '0') && (c <= '9'))
+		return (1);
+	else
+		return (0);
 
 	return (0);
 }
