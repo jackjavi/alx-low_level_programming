bec4bfc144c1bdc220b4815c8eeabcc4ba96fca6
jackjavi <jackmtembete@gmail.com>
2022-09-28 08:22:39 -0700

    check if prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,9 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int find_sqrt(int num, int root);
+int is_prime_number(int n);
+int is_divisible(int num, int div);
 
 #endif /* MAIN_H */
