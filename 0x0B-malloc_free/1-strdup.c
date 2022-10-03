1e5b73445c73c697c0a6afcdc99ced7a384ec5e4
jackjavi <jackmtembete@gmail.com>
2022-10-03 08:16:47 -0700

    Duplicate string with malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - Returns the length of a string
+ * #s: The string to get the length of.
+ *
+ * Return: the length of a string.
+ */
+int _strlen(char *s)
+{
+	int i, length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+
+
+	return (length);
+}
+
+/**
  * _strdup - returns a pointer to a new string which
  * is a duplicate of the string str.
  * @str: string to be duplicated
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,5 @@
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
-int _strlen(char *s);
 
 #endif /* MAIN_H */
