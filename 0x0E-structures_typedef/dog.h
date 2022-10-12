ae03032ea8e6921863a11c125b0be83b42841550
jackjavi <jackmtembete@gmail.com>
2022-10-12 10:24:35 -0700

    free dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+#include <stdlib.h>
 
 /**
  * struct dog - description of dog
@@ -17,5 +18,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
