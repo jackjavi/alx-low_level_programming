38d0998c1874e7dcd6588d083eb82489bdb8b559
jackjavi <jackmtembete@gmail.com>
2022-10-25 05:17:21 -0700

    Fixed error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,19 @@
 size_t print_list(const list_t *h)
 {
 	size_t count;
-	
+
 	count = 0;
-	if (h == NULL)
-		printf("[0] (nil)");
-	while (h != NULL)
+	while (h)
 	{
-		printf("[%u] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
 
