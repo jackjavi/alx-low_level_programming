d60cb3c9ba37d21c2511f37c78741c70ea8771ca
jackjavi <jackmtembete@gmail.com>
2023-04-13 19:43:57 -0700

    header files

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -37,7 +37,7 @@ typedef struct skiplist_s
     size_t index;
     struct skiplist_s *next;
     struct skiplist_s *express;
-} skiplist_t
+} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
